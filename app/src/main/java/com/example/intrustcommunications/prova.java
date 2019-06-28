552dd66010fbea2240b03ef9ab0924b06c3b665a
bryanbeffa <37347050+bryanbeffa@users.noreply.github.com>
2019-06-28 15:21:17 +0200

    added about us content

---
+++ app/src/main/java/com/example/intrustcommunications/prova.java
@@ -0,0 +1,14 @@
+package com.example.intrustcommunications;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class prova extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
