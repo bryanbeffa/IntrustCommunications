898fe02bc0ad32e2cfd0a9224871cadcf229675f
bryanbeffa <37347050+bryanbeffa@users.noreply.github.com>
2019-07-02 12:46:06 +0200

    added customer services

--- app/src/main/java/com/example/intrustcommunications/prova.java
+++ app/src/main/java/com/example/intrustcommunications/prova.java
@@ -9,6 +9,6 @@ public class prova extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_retention);
+        setContentView(R.layout.activity_back_office);
     }
 }
