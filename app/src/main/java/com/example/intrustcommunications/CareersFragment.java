1b458e20c9394ca9a9273591566b68e7a1d5acfd
bryanbeffa <37347050+bryanbeffa@users.noreply.github.com>
2019-07-03 14:26:33 +0200

    finish job parts

--- app/src/main/java/com/example/intrustcommunications/CareersFragment.java
+++ app/src/main/java/com/example/intrustcommunications/CareersFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -48,15 +49,26 @@ public class CareersFragment extends Fragment implements View.OnClickListener {
         //third job button
         TextView thirdJobButton = (TextView) view.findViewById(R.id.thirdJobButton);
         thirdJobButton.setOnClickListener(this);
+
+        //fourth job button
+        TextView fourthJobButton = (TextView) view.findViewById(R.id.fourthJobButton);
+        fourthJobButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
 
-        //set layouts
+        //get layouts
         RelativeLayout firstJobLayout = (RelativeLayout) getView().findViewById(R.id.firstJobContent);
         RelativeLayout secondJobLayout = (RelativeLayout) getView().findViewById(R.id.secondJobContent);
         RelativeLayout thirdJobLayout = (RelativeLayout) getView().findViewById(R.id.thirdJobContent);
+        RelativeLayout fourthJobLayout = (RelativeLayout) getView().findViewById(R.id.fourthJobContent);
+
+        //get buttons
+        TextView firstButton = (TextView) getView().findViewById(R.id.firstJobButton);
+        TextView secondButton = (TextView) getView().findViewById(R.id.secondJobButton);
+        TextView thirdButton = (TextView) getView().findViewById(R.id.thirdJobButton);
+        TextView fourthButton = (TextView) getView().findViewById(R.id.fourthJobButton);
 
         int id = view.getId();
 
@@ -69,11 +81,19 @@ public class CareersFragment extends Fragment implements View.OnClickListener {
 
                 //set layouts visibility
                 firstJobLayout.setVisibility(View.VISIBLE);
+                firstButton.setText("-");
                 secondJobLayout.setVisibility(View.GONE);
+                secondButton.setText("+");
                 thirdJobLayout.setVisibility(View.GONE);
+                thirdButton.setText("+");
+                fourthJobLayout.setVisibility(View.GONE);
+                fourthButton.setText("+");
 
             } else {
                 firstJobLayout.setVisibility(View.GONE);
+
+                //set button text
+                firstButton.setText("+");
             }
         } else if (id == R.id.secondJobButton) {
 
@@ -84,11 +104,19 @@ public class CareersFragment extends Fragment implements View.OnClickListener {
 
                 //set layouts visibility
                 firstJobLayout.setVisibility(View.GONE);
+                firstButton.setText("+");
                 secondJobLayout.setVisibility(View.VISIBLE);
+                secondButton.setText("-");
                 thirdJobLayout.setVisibility(View.GONE);
+                thirdButton.setText("+");
+                fourthJobLayout.setVisibility(View.GONE);
+                fourthButton.setText("+");
 
             } else {
                 secondJobLayout.setVisibility(View.GONE);
+
+                //set button text
+                secondButton.setText("+");
             }
         } else if (id == R.id.thirdJobButton) {
 
@@ -99,11 +127,43 @@ public class CareersFragment extends Fragment implements View.OnClickListener {
 
                 //set layouts visibility
                 firstJobLayout.setVisibility(View.GONE);
+                firstButton.setText("+");
                 secondJobLayout.setVisibility(View.GONE);
+                secondButton.setText("+");
                 thirdJobLayout.setVisibility(View.VISIBLE);
+                thirdButton.setText("-");
+                fourthJobLayout.setVisibility(View.GONE);
+                fourthButton.setText("+");
 
             } else {
                 thirdJobLayout.setVisibility(View.GONE);
+
+                //set button text
+                thirdButton.setText("+");
+
+            }
+        } else if (id == R.id.fourthJobButton) {
+
+            //get layout visibility
+            int visible = fourthJobLayout.getVisibility();
+
+            if(visible == View.GONE) {
+
+                //set layouts visibility
+                firstJobLayout.setVisibility(View.GONE);
+                firstButton.setText("+");
+                secondJobLayout.setVisibility(View.GONE);
+                secondButton.setText("+");
+                thirdJobLayout.setVisibility(View.GONE);
+                thirdButton.setText("+");
+                fourthJobLayout.setVisibility(View.VISIBLE);
+                fourthButton.setText("-");
+
+            } else {
+                fourthJobLayout.setVisibility(View.GONE);
+
+                //set button text
+                fourthButton.setText("+");
             }
         }
     }
