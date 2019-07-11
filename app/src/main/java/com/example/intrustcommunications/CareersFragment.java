b5ea6088beaf7958139cf50e9c5ce6867632f94d
bryanbeffa <37347050+bryanbeffa@users.noreply.github.com>
2019-07-11 12:49:44 +0200

    added alert dialog

--- app/src/main/java/com/example/intrustcommunications/CareersFragment.java
+++ app/src/main/java/com/example/intrustcommunications/CareersFragment.java
@@ -1,8 +1,12 @@
 package com.example.intrustcommunications;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -198,6 +202,13 @@ public class CareersFragment extends Fragment implements View.OnClickListener {
                     startActivity(Intent.createChooser(i, "Send mail..."));
                 } catch (android.content.ActivityNotFoundException ex) {
                 }
+            } else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Insert a subject and a massage")
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        }).show();
             }
         }
     }
