80205a49195fa2e6562297d3d4ec793cc646e86c
bryanbeffa <37347050+bryanbeffa@users.noreply.github.com>
2019-07-24 12:14:54 +0200

    fixed CurrentFragmentManager code

--- app/src/main/java/com/example/intrustcommunications/CurrentFragmentManager.java
+++ app/src/main/java/com/example/intrustcommunications/CurrentFragmentManager.java
@@ -1,16 +1,36 @@
 package com.example.intrustcommunications;
 
+/**
+ * Class used to define the current fragment.
+ *
+ * @author Bryan Beffa
+ * @version 01.07.2019
+ */
 public class CurrentFragmentManager {
     private int currentFragment;
 
-    public CurrentFragmentManager(){
+    /**
+     * Empty constructor.
+     * Set dashboard as default fragment.
+     */
+    public CurrentFragmentManager() {
         currentFragment = 0;
     }
 
+    /**
+     * Set the current fragment id.
+     *
+     * @param id fragment id.
+     */
     public void setCurrentFragment(int id) {
         currentFragment = id;
     }
 
+    /**
+     * Get the current fragment id.
+     *
+     * @return the id of the current fragment.
+     */
     public int getCurrentFragment() {
         return this.currentFragment;
     }
