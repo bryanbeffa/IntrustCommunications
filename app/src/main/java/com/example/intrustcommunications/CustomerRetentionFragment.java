7882383ed52dd7bf5392b12a70c9d90f3afa818e
bryanbeffa <37347050+bryanbeffa@users.noreply.github.com>
2019-07-24 12:19:05 +0200

    fixed CustomerRetentionFragment code

--- app/src/main/java/com/example/intrustcommunications/CustomerRetentionFragment.java
+++ app/src/main/java/com/example/intrustcommunications/CustomerRetentionFragment.java
@@ -1,6 +1,5 @@
 package com.example.intrustcommunications;
 
-
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,24 +12,48 @@ import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
-
 /**
- * A simple {@link Fragment} subclass.
+ * @author Bryan Beffa
+ * @version 01.07.2019
  */
 public class CustomerRetentionFragment extends Fragment implements View.OnClickListener {
 
+    /**
+     * Constant that defines the contact us id.
+     * Default value 6.
+     */
     private final int CONTACT_US_ID = 6;
+
+    /**
+     * Attribute that defines the CurrentFragmentManager object.
+     * Used to set the current item in the menu.
+     */
     private CurrentFragmentManager cfManager;
+
+    /**
+     * Attribute that defines the navigationView.
+     */
     private NavigationView navigationView;
 
+    /**
+     * Constructor that requires the CurrentFragmentManager and the NavigationView objects.
+     *
+     * @param cfManager      CurrentFragmentManager to set to cfManager attribute.
+     * @param navigationView NavigationView to set to navigationView attribute.
+     */
     public CustomerRetentionFragment(CurrentFragmentManager cfManager, NavigationView navigationView) {
         this.cfManager = cfManager;
         this.navigationView = navigationView;
     }
 
+    /**
+     * Method that set the listeners to the elements.
+     *
+     * @param view that contains the elements.
+     */
     private void setOnClickListeners(View view) {
         //first job button
-        TextView contactUsButton = (TextView) view.findViewById(R.id.contactUsButton);
+        TextView contactUsButton = view.findViewById(R.id.contactUsButton);
         contactUsButton.setOnClickListener(this);
     }
 
@@ -48,13 +71,17 @@ public class CustomerRetentionFragment extends Fragment implements View.OnClickL
 
     @Override
     public void onClick(View view) {
+
+        //get view id
         int id = view.getId();
 
+        //check if the contact us button is clicked
         if (id == R.id.contactUsButton) {
 
             //check if is the current fragment
             if (cfManager.getCurrentFragment() != CONTACT_US_ID) {
 
+                //replace the current fragment with the contact us fragment
                 FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
                 ft.setCustomAnimations(R.anim.slide_in_right, 0);
                 ft.replace(R.id.flMain, new ContactUsFragment(cfManager, navigationView));
